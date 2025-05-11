5b590d8858653dcc206c96c6d152272fc8babbb4
akdevv <akdev5520@gmail.com>
2025-05-11 11:07:52 +0530

    Implement authentication features: add Google login functionality to login and register forms, enforce session checks on chat page, and update dependencies in package.json and bun.lock.

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -9,12 +9,33 @@ import {
 	CardTitle,
 } from "@/components/ui/card";
 import { FaGoogle } from "react-icons/fa";
-import { useRouter } from "next/navigation";
+import { FiLoader } from "react-icons/fi";
+import { MdOutlineErrorOutline } from "react-icons/md";
 import { Button } from "@/components/ui/button";
 
-export default function LoginForm() {
-	const router = useRouter();
+import Link from "next/link";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
 
+export default function LoginForm() {
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [error, setError] = useState<string | null>(null);
+
+	const handleGoogleLogin = async () => {
+		setIsLoading(true);
+		try {
+			console.log("Signing in with Google");
+			const res = await signIn("google", {
+				callbackUrl: "/chat",
+			});
+			console.log("res", res);
+		} catch (error) {
+			console.error(error);
+			setError("Failed to sign in with Google");
+		} finally {
+			setIsLoading(false);
+		}
+	};
 	return (
 		<Card className=" bg-background-light border-none rounded-lg p-4 mx-auto max-w-sm">
 			<CardHeader className="space-y-1">
@@ -28,12 +49,29 @@ export default function LoginForm() {
 			<CardContent className="grid gap-4">
 				<Button
 					variant="outline"
-					onClick={() => {}}
+					onClick={handleGoogleLogin}
+					disabled={isLoading}
 					className="bg-background text-foreground border-none hover:bg-black hover:text-foreground-dark"
 				>
-					<FaGoogle className="mr-2" />
-					<span className="font-bold">Continue with Google</span>
+					{isLoading ? (
+						<FiLoader className="mr-2 animate-spin" />
+					) : (
+						<>
+							<FaGoogle className="mr-2" />
+							<span className="font-bold">
+								Continue with Google
+							</span>
+						</>
+					)}
 				</Button>
+				{error && (
+					<div className="flex items-center justify-center gap-2">
+						<MdOutlineErrorOutline className="text-xl text-red-500" />
+						<p className="text-center text-sm text-red-500">
+							{error || "Something went wrong!"}
+						</p>
+					</div>
+				)}
 			</CardContent>
 			<CardFooter className="flex flex-col items-center">
 				<p className="text-center text-sm text-foreground-dark mb-2">
@@ -42,9 +80,9 @@ export default function LoginForm() {
 				<Button
 					variant="link"
 					className="text-accent-blue hover:text-accent-blue-hover"
-					onClick={() => router.push("/auth/register")}
+					asChild
 				>
-					Register
+					<Link href="/auth/register">Register</Link>
 				</Button>
 			</CardFooter>
 		</Card>
--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -9,11 +9,33 @@ import {
 	CardTitle,
 } from "@/components/ui/card";
 import { FaGoogle } from "react-icons/fa";
-import { useRouter } from "next/navigation";
+import { FiLoader } from "react-icons/fi";
+import { MdOutlineErrorOutline } from "react-icons/md";
 import { Button } from "@/components/ui/button";
 
+import Link from "next/link";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+
 export default function RegisterForm() {
-	const router = useRouter();
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [error, setError] = useState<string | null>(null);
+
+	const handleGoogleRegister = async () => {
+		setIsLoading(true);
+		try {
+			console.log("Signing in with Google");
+			const res = await signIn("google", {
+				callbackUrl: "/chat",
+			});
+			console.log("res", res);
+		} catch (error) {
+			console.error(error);
+			setError("Failed to sign in with Google");
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
 	return (
 		<Card className=" bg-background-light border-none rounded-lg p-4 mx-auto max-w-sm">
@@ -28,12 +50,29 @@ export default function RegisterForm() {
 			<CardContent className="grid gap-4">
 				<Button
 					variant="outline"
-					onClick={() => {}}
+					onClick={handleGoogleRegister}
+					disabled={isLoading}
 					className="bg-background text-foreground border-none hover:bg-black hover:text-foreground-dark"
 				>
-					<FaGoogle className="mr-2" />
-					<span className="font-bold">Continue with Google</span>
+					{isLoading ? (
+						<FiLoader className="mr-2 animate-spin" />
+					) : (
+						<>
+							<FaGoogle className="mr-2" />
+							<span className="font-bold">
+								Continue with Google
+							</span>
+						</>
+					)}
 				</Button>
+				{error && (
+					<div className="flex items-center justify-center gap-2">
+						<MdOutlineErrorOutline className="text-xl text-red-500" />
+						<p className="text-center text-sm text-red-500">
+							{error || "Something went wrong!"}
+						</p>
+					</div>
+				)}
 			</CardContent>
 			<CardFooter className="flex flex-col items-center">
 				<p className="text-center text-sm text-foreground-dark mb-2">
@@ -41,10 +80,11 @@ export default function RegisterForm() {
 				</p>
 				<Button
 					variant="link"
+					disabled={isLoading}
 					className="text-accent-blue hover:text-accent-blue-hover"
-					onClick={() => router.push("/auth/login")}
+					asChild
 				>
-					Login
+					<Link href="/auth/login">Login</Link>
 				</Button>
 			</CardFooter>
 		</Card>
